a9be21fff2f9d9c3ec506474c6d7f682abe594cc
Fabian Goźliński (Uczeń) <fgozlinski546@technikumkreatywne.pl>
2023-10-23 13:05:15 +0200

    Pierwsza lekcja

---
+++ 1-struktura-pliku/Panie Jakubie niech mnie pan nie bije.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	
+	short zmienna =  7;	
+	
+	
+	cout << "Hello World, Bonjour Bonjour!!!!! " << zmienna;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
