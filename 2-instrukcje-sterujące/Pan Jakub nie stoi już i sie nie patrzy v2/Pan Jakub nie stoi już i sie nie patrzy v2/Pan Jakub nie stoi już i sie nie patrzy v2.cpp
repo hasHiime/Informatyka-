be0d60e333923c7e6b0747da8dae57a29c809249
Fabian Goźliński (Uczeń) <fgozlinski546@technikumkreatywne.pl>
2023-11-06 13:16:47 +0100

    Pętelki

---
+++ 2-instrukcje-sterujące/Pan Jakub nie stoi już i sie nie patrzy v2/Pan Jakub nie stoi już i sie nie patrzy v2/Pan Jakub nie stoi już i sie nie patrzy v2.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+	// Całkowite- int, short, long, unsigned short
+	short a, b;
+
+	cout << "Podaj a: ";
+	cin >> a;
+	cout << "Podaj b: ";
+	cin >> b;
+
+
+	// Instrukcja warunkowa - if(warunek)
+	if (a < b) {
+		cout << a << " < " << b;
+	}
+	else if (a == b) {
+		cout << a << " == " << b;
+	}
+
+
+
+	else {
+		cout << a << " > " << b;
+	}
+
+
+
+
+
+
+
+
+
+}
---
+++ 3- pętle/Pętelki z panem Jakbuem 1.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	/*
+	Inkrementacja
+	 
+	i = i + 1 
+	 
+	i += 1
+	 
+	i++
+	
+	Dekrementacja
+	
+	i = i - 1
+	i--
+	*/
+	
+	// for(wartośćPoczątkowa; warunekKończący; krok)
+	
+	for(int i=0; i <= 5; i++){   // i - zmienna lokalna, istnieje tylko w pętli
+		cout << "Wartosc i: " << i << endl;
+ 	}
+	
+	
+	int a;
+	int b;
+	
+	cout << "Podaj wysokosc";
+	cin >> a;
+	cout << "Podaj długosc";
+	cin >> b;
+	
+	
+	
+	
+	
+	for(int i=0; i<a; i++){
+		for(int j=0; j<b; j++){
+			cout <<  " " << i << j;
+			
+			
+		}
+		cout << endl;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	cout << endl;
+	system("pause");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
