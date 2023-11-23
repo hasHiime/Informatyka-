92e6c8b61bd1d18b8dd758c06592e3c18803d896
Fabian Goźliński (Uczeń) <fgozlinski546@technikumkreatywne.pl>
2023-11-23 10:57:35 +0100

    Pętelka kolejna

---
+++ 3- pętle/Pan Jakub pętelkuje/Pan Jakub pętelkuje/Pan Jakub pętelkuje.cpp
@@ -0,0 +1,21 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    short a = 30;
+    while (a >= 10) {
+        cout << a << ", ";
+        a -= 2;
+    }
+    float liczba;
+    cout << "Podaj liczbe: ";
+    cin >> liczba;
+    do {
+        cout << liczba << ", ";
+        liczba = liczba / 2;
+    }while(liczba > 0);
+    cout << endl;
+    system("pause");
+}
